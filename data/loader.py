518e4400ce6b4e521a621247a6e7f55314ad63d3
dgedon <daniel.gedon@gmx.de>
2020-06-26 09:27:23 +0200

    final cleanup

--- data/loader.py
+++ data/loader.py
@@ -1,13 +1,14 @@
 from data.base import DataLoaderExt
-from data.cascaded_tank import create_cascadedtank_datasets
-from data.f16gvt import create_f16gvt_datasets
+# from data.cascaded_tank import create_cascadedtank_datasets
+# from data.f16gvt import create_f16gvt_datasets
 from data.narendra_li import create_narendra_li_datasets
 from data.toy_lgssm import create_toy_lgssm_datasets
 from data.wiener_hammerstein import create_wienerhammerstein_datasets
 
 
 def load_dataset(dataset, dataset_options, train_batch_size, test_batch_size, **kwargs):
-    if dataset == 'cascaded_tank':
+    """Not used datasets: F16 and Cascadedtank"""
+    """if dataset == 'cascaded_tank':
         dataset_train, dataset_valid, dataset_test = create_cascadedtank_datasets(dataset_options.seq_len_train,
                                                                                   dataset_options.seq_len_val,
                                                                                   dataset_options.seq_len_test)
@@ -23,8 +24,8 @@ def load_dataset(dataset, dataset_options, train_batch_size, test_batch_size, **
         # Dataloader
         loader_train = DataLoaderExt(dataset_train, batch_size=train_batch_size, shuffle=True, num_workers=1)
         loader_valid = DataLoaderExt(dataset_valid, batch_size=test_batch_size, shuffle=False, num_workers=1)
-        loader_test = DataLoaderExt(dataset_test, batch_size=test_batch_size, shuffle=False, num_workers=1)
-    elif dataset == 'narendra_li':
+        loader_test = DataLoaderExt(dataset_test, batch_size=test_batch_size, shuffle=False, num_workers=1)"""
+    if dataset == 'narendra_li':
         dataset_train, dataset_valid, dataset_test = create_narendra_li_datasets(dataset_options.seq_len_train,
                                                                                  dataset_options.seq_len_val,
                                                                                  dataset_options.seq_len_test,
